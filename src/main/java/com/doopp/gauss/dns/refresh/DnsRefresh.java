695596365932143cdafb3086f9f11b648f5e6df4
liuyi <yi.liu@ushow.media>
2018-02-14 11:56:57 +0800

    完成功能，给了一个打好包

--- src/main/java/com/doopp/gauss/dns/refresh/DnsRefresh.java
+++ src/main/java/com/doopp/gauss/dns/refresh/DnsRefresh.java
@@ -2,8 +2,7 @@ package com.doopp.gauss.dns.refresh;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordRequest;
-import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordResponse;
+import com.aliyuncs.alidns.model.v20150109.*;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
@@ -14,31 +13,37 @@ import com.ning.http.client.Response;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.Future;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DnsRefresh implements Runnable {
 
     private String propertyFile = "";
 
-    DnsRefresh(String propertyFile)
-    {
+    DnsRefresh(String propertyFile) {
         this.propertyFile = propertyFile;
     }
 
     @Override
     public void run() {
 
-        System.out.print(" >>> " + propertyFile);
+        System.out.print(" >>> " + propertyFile + "\n");
         Properties properties = this.getProperties();
-        if (properties==null) {
+        if (properties == null) {
             return;
         }
 
+        // 要动态解析的子域名列表
+        String[] domainArray = properties.getProperty("refreshDomain").split(",");
+        // 旧的 IP
         String oldIp = properties.getProperty("oldIp");
+        // IP 查询服务器
         String requestUrl = properties.getProperty("ipQueryServer");
-
-        String recordId = properties.getProperty("recordId");
+        // 一级域名
+        String rootDomain = properties.getProperty("rootDomain");
+        // aliyun 配置
         String regionId = properties.getProperty("regionId");
         String accessKeyId = properties.getProperty("accessKeyId");
         String accessKeySecret = properties.getProperty("accessKeySecret");
@@ -48,34 +53,64 @@ public class DnsRefresh implements Runnable {
         // DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Alidns", "alidns.aliyuncs.com");
         IAcsClient client = new DefaultAcsClient(profile);
 
-        while (true) {
-            String newIp = this.httpGet(requestUrl);
-            System.out.print(" >>> new ip : " + newIp + "\n");
+        // recordId 对应域名的列表
+        Map<String, String> recordRRList = new HashMap<>();
 
-            if (newIp!=null && !newIp.equals(oldIp)) {
-
-                UpdateDomainRecordRequest updateRequest = new UpdateDomainRecordRequest();
-                updateRequest.setRecordId(recordId);
-                updateRequest.setRR("home");
-                updateRequest.setType("A");
-                updateRequest.setValue(newIp);
-
-                try {
-                    UpdateDomainRecordResponse updateResponse = client.getAcsResponse(updateRequest);
-                    System.out.println("UpdateDomainRecordResponse : " + updateResponse);
+        // 获取子域名
+        DescribeDomainRecordsRequest request = new DescribeDomainRecordsRequest();
+        request.setDomainName(rootDomain);
+        try {
+            DescribeDomainRecordsResponse response = client.getAcsResponse(request);
+            List<DescribeDomainRecordsResponse.Record> recordList = response.getDomainRecords();
+            for (DescribeDomainRecordsResponse.Record record : recordList) {
+                if (Arrays.asList(domainArray).contains(record.getRR())) {
+                    // 取得要解析的 record id
+                    recordRRList.put(record.getRecordId(), record.getRR());
                 }
-                catch (ClientException e) {
-                    System.out.println("client.getAcsResponse Error : " + e.getMessage() + "\n");
-                }
-                oldIp = newIp;
             }
+        } catch (ClientException e) {
+            System.out.print(" >>> Error : "+ e.getMessage() + "\n");
+        }
 
-            synchronized (this) {
-                try {
-                    this.wait(10 * 1000);
+        // 新的 IP 先和旧 IP 一样
+        String newIp = oldIp;
+
+        // 初始化更新域名解析的类
+        UpdateDomainRecordRequest updateRequest = new UpdateDomainRecordRequest();
+        updateRequest.setType("A");
+
+        // 每一段时间循环一次
+        while (true) {
+            String ipHtml = this.httpGet(requestUrl);
+            if (ipHtml != null) {
+                Pattern p = Pattern.compile("(\\d+\\.\\d+\\.\\d+\\.\\d+)");
+                Matcher m = p.matcher(ipHtml);
+                if (m.find()) {
+                    newIp = m.group(1);
                 }
-                catch (Exception e) {
-                    System.out.println("System wait error : " + e.getMessage() + "\n");
+                // 如果 IP 发生了变化
+                if (newIp != null && !newIp.equals(oldIp)) {
+                    updateRequest.setValue(newIp);
+                    // 将每个要解析的域名都处理一次
+                    for (String recordId : recordRRList.keySet()) {
+                        updateRequest.setRR(recordRRList.get(recordId));
+                        updateRequest.setRecordId(recordId);
+                        try {
+                            UpdateDomainRecordResponse updateResponse = client.getAcsResponse(updateRequest);
+                            System.out.println("UpdateDomainRecordResponse : " + updateResponse + "\n");
+                        } catch (ClientException e) {
+                            System.out.println("client.getAcsResponse Error : " + e.getMessage() + "\n");
+                        }
+                    }
+                    oldIp = newIp;
+                }
+
+                synchronized (this) {
+                    try {
+                        this.wait(10 * 1000);
+                    } catch (Exception e) {
+                        System.out.println("System wait error : " + e.getMessage() + "\n");
+                    }
                 }
             }
         }
@@ -91,8 +126,7 @@ public class DnsRefresh implements Runnable {
         });
         try {
             return f.get();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("can not http request " + requestUrl + "\n");
         }
         return null;
@@ -105,9 +139,8 @@ public class DnsRefresh implements Runnable {
             Properties pros = new Properties();
             pros.load(is);
             return pros;
-        }
-        catch (IOException e) {
-            System.out.println( propertyFile + " is can not read \n");
+        } catch (IOException e) {
+            System.out.println(propertyFile + " is can not read \n");
         }
         return null;
     }
